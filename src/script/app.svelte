73b9c194e508beba31c0bceaefdefc5b50fd2747
Fredrik Gärdin <fredrik.gardin@ninetech.se>
2019-06-06 01:35:37 +0200

    initial commit

---
+++ src/script/app.svelte
@@ -0,0 +1,13 @@
+<script>
+    const nname = 'world!!';
+</script>
+
+<style>
+    h1 {
+        background: green;
+        color: rgb(250, 250, 244);
+        font-family: 'Courier New', Courier, monospace
+    }
+</style>
+
+<h1>Hello {nname}</h1>
